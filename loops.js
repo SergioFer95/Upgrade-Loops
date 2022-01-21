45e4d3e34f358280d6e0805bc99cf48e49f61243
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-01-21 21:59:43 +0100

    Resolve Iteration 1

--- loops.js
+++ loops.js
@@ -4,6 +4,15 @@
 // Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta". Usa la función .***includes*** de javascript.
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 
+for (const clothing of products) {
+    let getClothes = clothing;
+
+    if(getClothes.includes('Camiseta')){
+        console.log(getClothes);
+    }
+    
+}
+
 
 
 // Iteración #2: Condicionales avanzados
@@ -22,7 +31,7 @@ const alumns = [
 //Iteración #3: Probando For...of
 // Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
 // Puedes usar este array:
-const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+const placesToTravelOne = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
 
 
 
@@ -39,13 +48,13 @@ const alien = {
 
 // Iteración #5: Probando For
 // Usa un bucle **for** para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en un ***console log*** el array. Puedes usar este array:
-const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}];
+const placesToTravelTwo = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}];
 
 
 
 // Iteración #6: Mixed For...of e includes
-// Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función ***.includes()*** para comprobarlo.Puedes usar este array:
-const toys = [
+// Usa un bucle **for...of** para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función ***.includes()*** para comprobarlo. Puedes usar este array:
+const toysOne = [
     {id: 5, name: 'Buzz MyYear'}, 
     {id: 11, name: 'Action Woman'}, 
     {id: 23, name: 'Barbie Man'}, 
